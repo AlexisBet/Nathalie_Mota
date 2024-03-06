00254db514e9968c3537d92b974955375662bb2f
Betunio Alexis <betunio.alexis2@gmail.com>
2024-03-06 19:19:37 +0100

    Ajout des fichiers header.php et footer.php

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <header>
+        <img src="<?php echo get_template_directory_uri() . '/images/NathalieMota.png'; ?>" alt="Logo de Nathalie Mota Photographe">
+    </header>
+</body>
+</html>
